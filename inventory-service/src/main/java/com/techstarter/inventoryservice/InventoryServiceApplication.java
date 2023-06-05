dc487c482b9f80a87cbc7e87b869700bafc4fdae
girish-thetechieguy <girish.thetechieguy@gmail.com>
2023-06-06 00:27:15 +0800

    added the merge conflicts and push

---
+++ inventory-service/src/main/java/com/techstarter/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techstarter.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
